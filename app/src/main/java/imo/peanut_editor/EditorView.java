edd32a83a7fcbf59654e28d83f023b8dc8467e24
IMO <180146096+IMOitself@users.noreply.github.com>
2026-03-01 20:49:11 +0800

    feat: adapt custom view text color to theme

--- app/src/main/java/imo/peanut_editor/EditorView.java
+++ app/src/main/java/imo/peanut_editor/EditorView.java
@@ -1,10 +1,12 @@
 package imo.peanut_editor;
 
 import android.content.Context;
-import android.view.View;
-import android.util.AttributeSet;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
 
 public class EditorView extends View {
     public EditorView(Context context) {
@@ -25,10 +27,17 @@ public class EditorView extends View {
     private Paint mPaint;
     private String mText = "Hello World";
     private float mTextSize = 30f;
+	private int mTextColor = -1;
 
     private void init() {
         mPaint = new Paint();
         mPaint.setTextSize(mTextSize);
+		
+		TypedArray typedArray = getContext().obtainStyledAttributes(new int[]{android.R.attr.textColor});
+		mTextColor = typedArray.getColor(0, Color.RED);
+		typedArray.recycle();
+		
+		mPaint.setColor(mTextColor);
     }
 
     @Override
