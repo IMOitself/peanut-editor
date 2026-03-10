082c035282ca72f7fc6934ca01bbd02134b92c46
IMO <180146096+IMOitself@users.noreply.github.com>
2026-03-10 18:56:08 +0800

    feat: draw letters one by one repeatedly with 1s delay

--- app/src/main/java/imo/peanut_editor/EditorView.java
+++ app/src/main/java/imo/peanut_editor/EditorView.java
@@ -26,9 +26,10 @@ public class EditorView extends View {
     }
 
     private Paint mPaint;
-    private String mText = "Hello World";
+    private String mText = "beep boop ";
     private float mTextSize = 30f;
 	private int mTextColor = -1;
+    private int mLettersToDrawSoFar = 1;
 
     private void init() {
         mPaint = new Paint();
@@ -45,8 +46,25 @@ public class EditorView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        float letterWidth = mPaint.measureText(" ");
         float x = 0;
         float y = -mPaint.ascent(); // cant be 0 because drawing the text start from baseline
-        canvas.drawText(mText, x, y, mPaint);
+        
+        for(int i = 0; i < mLettersToDrawSoFar; i++) { // draw all letters allowed to show so far
+            char letter = mText.charAt(i % mText.length());
+
+            canvas.drawText(letter+"", x, y, mPaint);
+
+            x += letterWidth;
+        }
+
+        if (x < getWidth()) { // not hit the right edge yet
+            mLettersToDrawSoFar++;
+            postInvalidateDelayed(1000);
+            return;
+        }
+        // else
+        // TODO: draw how many letters has been drawn on another line
+        
     }
 }
